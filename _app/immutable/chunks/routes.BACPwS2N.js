db578461ed94d5d3897563c524046b4e689abf61
Harry Xu <92757553+HarryXu497@users.noreply.github.com>
2024-06-22 21:39:27 -0400

    Updates

---
+++ _app/immutable/chunks/routes.BACPwS2N.js
@@ -0,0 +1,1 @@
+const t=[{title:"Home",url:"/"},{title:"FAQ",url:"/faq"},{title:"Groups",url:"/groups"},{title:"Contest",url:"/contest"},{title:"Web",url:"/web"},{title:"Workshops",url:"/workshops"},{title:"Events",url:"/events"}];export{t as r};
---
+++ _app/immutable/entry/start.D7KrWXol.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.V_muOjlG.js";export{t as start};
